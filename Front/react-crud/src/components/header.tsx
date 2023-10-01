80491373b60c8b2e4bc3636bc6cbd6d2facf1b43
LEE DONG YEONG <kinzinzz@naver.com>
2023-10-01 22:24:13 +0900

    Front update

--- Front/react-crud/src/components/header.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'react-bootstrap/Image';
-import './header.css'
-
-function Header() {
-    return (
-        <Image className="indexImage" src="/indexImage.jpg" />
-    )
-};
-export default Header;
--- Front/react-crud/src/index.tsx
+++ Front/react-crud/src/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
-import NavBar from './components/navbar';
-import Header from './components/header';
-import Posts from './components/posts/posts';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <NavBar />
-    <Header />
-    <Posts />
+    <App />
   </React.StrictMode>
 );
 
